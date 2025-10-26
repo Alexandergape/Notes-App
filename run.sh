d5a81330d934d6c426b2200312cab6a1415ab4f2
Alexander Peñaloza <apenaloza@territoriosinteligentes.net>
2025-10-26 16:13:39 -0500

    REF refactor variables

--- run.sh
+++ run.sh
@@ -4,7 +4,12 @@ echo "🧹 Cleaning old containers..."
 docker-compose down #-v   # remove containers + volumes (optional: -v removes pgdata)
 
 echo "🚀 Starting Notes App stack..."
-docker-compose up -d #--build
+
+VM_IP=$(curl -s ifconfig.me)
+echo "🌐 VM IP detected: $VM_IP"
+VITE_API_URL="http://${VM_IP}:8080" docker-compose up -d --build
+
+# docker-compose up -d #--build
 
 echo "✅ All services are running!"
 echo "Frontend: http://localhost:5173"
