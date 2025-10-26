d8cdd8bb55bd29d875ceef3c3c9a25fb105d8d6b
Alexander Peñaloza <apenaloza@territoriosinteligentes.net>
2025-10-26 16:27:48 -0500

    FIX: fix api call

--- frontend/src/features/categories/categoriesApi.ts
+++ frontend/src/features/categories/categoriesApi.ts
@@ -1,18 +1,18 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import type { Category } from './categoriesSlice'
-import axios from 'axios'
+import { api } from '../../api/api'
 
 export const fetchCategories = createAsyncThunk('categories/fetchAll', async () => {
-  const response = await axios.get('http://localhost:8080/categories')
+  const response = await api.get('/categories')
   return response.data as Category[]
 })
 
 export const createCategory = createAsyncThunk('categories/create', async (name: string) => {
-  const response = await axios.post('http://localhost:8080/categories', { name })
+  const response = await api.post('/categories', { name })
   return response.data as Category
 })
 
 export const deleteCategory = createAsyncThunk('categories/delete', async (id: number) => {
-  await axios.delete(`http://localhost:8080/categories/${id}`)
+  await api.delete(`/categories/${id}`)
   return id
 })
